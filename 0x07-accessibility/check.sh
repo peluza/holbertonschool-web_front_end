87d9ffee4b3e14fb1f50ac088cc1b92d00c239ba
peluza <edisonisaza@gmail.com>
2021-01-14 19:30:14 -0500

    check

---
+++ 0x07-accessibility/check.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+"keyboard/01-styles.css, keyboard/01-index.html" "skip-links/01-index.html, skip-links/01-article.html, skip-links/01-styles.css" "fix-a11y/01-index.html" "fix-a11y/02-index.html" "fix-a11y/03-index.html" "fix-a11y/04-index.html" "fix-a11y/05-index.html" "fix-a11y/06-index.html" "fix-a11y/07-index.html" "fix-a11y/08-index.html" "fix-a11y/09-index.html" "fix-a11y/10-index.html" 
