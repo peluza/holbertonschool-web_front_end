3b9a6293d70e02df41ee417ad43628406494161d
peluza <edisonisaza@gmail.com>
2021-02-16 14:30:44 -0500

    Closure

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function welcomeMessage(fullName) {
+  const message = "Welcome " + fullName;
+  alert(message);
+}
+const guillaume = welcomeMessage("Guillaume");
+const alex = welcomeMessage("Alex");
+const fred = welcomeMessage("Fred");
