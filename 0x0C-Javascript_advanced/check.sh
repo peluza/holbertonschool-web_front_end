d2a45c65391231c7b3f1ac862960c422b808dcf5
peluza <edisonisaza@gmail.com>
2021-02-16 14:33:05 -0500

    check.sh

---
+++ 0x0C-Javascript_advanced/check.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+semistandard --fix "0-welcome.js" "1-nested_functions.js" "2-function_me.js" "3-classrooms.js" "4-math.js" "5-mode.js" "6-hogwarts.js" "7-timeout.js" "8-payments.js" "9-prime.js" "10-prime.js" "11-prime.js" "12-room_area.js" "13-bind_user.js" "14-wikipedia.js" "100-stock.js" 
