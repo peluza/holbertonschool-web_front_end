0d4ee3d3a704ad6914cd7b2c194591d2bf4075f7
peluza <edisonisaza@gmail.com>
2021-02-17 18:36:28 -0500

    Execution stack & timing execution

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+function countPrimeNumbers() {
+  let isPrime;
+  let countPrimes = 0;
+  for (let i = 2; i <= 100; i++) {
+    isPrime = true;
+    let end = Math.round(Math.sqrt(i));
+    for (let mod = 2; mod <= end; mod++) {
+      if (i % mod == 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      countPrimes = countPrimes + 1;
+    }
+  }
+  console.log(countPrimes);
+  return countPrimes;
+}
+let startCountPrimeNumbers = window.performance.now();
+for (let j = 1; j < 101; j++) {
+  countPrimeNumbers();
+}
+let endCountPrimeNumbers = window.performance.now();
+
+console.log(
+  "Execution time of calculating prime numbers 100 times was " +
+    (endCountPrimeNumbers - startCountPrimeNumbers) +
+    " milliseconds."
+);
